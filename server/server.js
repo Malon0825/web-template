26d2372da2e983d441314cd2b4ca9333a832594e
catun <catun@ARAARA>
2024-09-04 22:09:09 +0800

    added functions for server side

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+const userRouter = require("./routes/users");
+
+app.use("/users", userRouter);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
