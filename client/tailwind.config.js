a4f3c99968f26bf14af10af66d3ec250e96a9664
mark.malon.s.catunao <mark.catunao@newbrighton.edu.ph>
2024-09-05 13:01:38 +0800

    added vite to client app

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,9 @@
-// tailwind.config.js
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
